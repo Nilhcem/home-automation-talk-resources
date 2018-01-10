fb1c22d3a3af78c30028734ee2302ea968f1f5da
Gautier Nilhcem <nilhcem@gmail.com>
2018-01-10 21:30:58 +0100

    Add firebase authentication

--- android-things-app/README.md
+++ android-things-app/README.md
@@ -6,3 +6,15 @@
 * Add an Android app to this project, with the following package name: `com.nilhcem.androidthings.homeautomation`
 * Download the `google-services.json` file and save it to the `app` directory
 * Create a new Cloud Firestore Database
+* Enable Email/Password Authentication and add a user via the Firebase console
+* Modify the `FIREBASE_EMAIL` and `FIREBASE_PASSWORD` from the `app/build.gradle`, optionally reduce the sign-up quota
+* Update your Firestore Security Rules
+```
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /users/{userId}/{document=**} {
+      allow read, write: if request.auth.uid == userId;
+    }
+  }
+}
+```
--- android-things-app/app/src/main/java/com/nilhcem/androidthings/homeautomation/device/data/FirestoreLiveData.kt
+++ android-things-app/app/src/main/java/com/nilhcem/androidthings/homeautomation/device/data/FirestoreLiveData.kt
@@ -2,22 +2,59 @@ package com.nilhcem.androidthings.homeautomation.device.data
 
 import android.arch.lifecycle.LiveData
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
+import com.nilhcem.androidthings.homeautomation.BuildConfig
 import com.nilhcem.androidthings.homeautomation.device.data.model.Device
 import com.nilhcem.androidthings.homeautomation.device.data.model.Lightbulb
 import com.nilhcem.androidthings.homeautomation.device.data.model.Unknown
 
 class FirestoreLiveData : LiveData<Device>() {
 
-    private lateinit var listener: ListenerRegistration
+    companion object {
+        private val TAG = FirestoreLiveData::class.java.simpleName!!
+    }
+
+    private var listener: ListenerRegistration? = null
 
     override fun onActive() {
         super.onActive()
+        authenticate { user ->
+            listenForFirestoreChanges(user)
+        }
+    }
 
-        val ref = FirebaseFirestore.getInstance().collection("devices")
-        ref.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+    override fun onInactive() {
+        super.onInactive()
+        listener?.remove().also { listener = null }
+    }
+
+    private fun authenticate(onceAuthenticated: (FirebaseUser) -> Unit) {
+        val auth = FirebaseAuth.getInstance()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            Log.d(TAG, "User is not authenticated")
+            auth.signInWithEmailAndPassword(BuildConfig.FIREBASE_EMAIL, BuildConfig.FIREBASE_PASSWORD)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "signInWithEmail:success")
+                            onceAuthenticated(task.result.user)
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        }
+                    }
+        } else {
+            Log.d(TAG, "User is already authenticated")
+            onceAuthenticated(currentUser)
+        }
+    }
+
+    private fun listenForFirestoreChanges(user: FirebaseUser) {
+        val ref = FirebaseFirestore.getInstance().collection("users").document(user.uid).collection("devices")
+        listener = ref.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
             if (firebaseFirestoreException == null) {
                 querySnapshot.documentChanges.forEach { documentChange ->
                     if (documentChange.type == DocumentChange.Type.MODIFIED) {
@@ -32,13 +69,8 @@ class FirestoreLiveData : LiveData<Device>() {
                     }
                 }
             } else {
-                Log.e(FirestoreLiveData::class.java.simpleName!!, "Firestore error", firebaseFirestoreException)
+                Log.e(TAG, "Firestore error", firebaseFirestoreException)
             }
         }
-    }
-
-    override fun onInactive() {
-        super.onInactive()
-        listener.remove()
     }
 }
