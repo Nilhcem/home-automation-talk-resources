9105b17f7c88fd8351bbea787bb8e9df453f80f8
Gautier Nilhcem <nilhcem@gmail.com>
2018-01-13 23:07:33 +0100

    Handle Actions on Google intents

--- google-cloud-function/app.js
+++ google-cloud-function/app.js
@@ -1,3 +1,6 @@
+// User Id in Firestore
+const userId = 'tP7PVhRDNHTgznofXFJPS6VDem83';
+
 // Initialize Cloud Firestore
 const admin = require('firebase-admin');
 admin.initializeApp({
@@ -5,34 +8,140 @@ admin.initializeApp({
 });
 var db = admin.firestore();
 
-// User Id in Firestore
-const userId = 'user-id';
-
+// HTTP POST /ha
 exports.ha = function(req, res) {
-  console.log(`About to get data for userId: ${userId}`);
-  var docRef = db.doc(`users/${userId}/devices/lightbulb`);
+  let reqBody = req.body;
+  let authToken = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+  console.log(`/ha called by Bearer ${authToken}`, reqBody);
 
-  docRef.get()
-      .then(doc => {
-          if (!doc.exists) {
-            console.log('No such document!');
-          } else {
-            var data = doc.data();
-            console.log('Document data:', data);
-
-            if (req.query.on) {
-              data.on = req.query.on === 'true';
-            }
-
-            if (req.query.spectrumRGB) {
-              data.spectrumRGB = req.query.spectrumRGB;
-            }
-
-            docRef.set(data);
-            res.send('OK!');
-          }
-      })
-      .catch(err => {
-          console.log('Error getting document', err);
-      });
+  switch (reqBody.inputs[0].intent) {
+    case 'action.devices.SYNC':
+      sync(reqBody, res);
+      break;
+    case 'action.devices.QUERY':
+      query(reqBody, res);
+      break;
+    case 'action.devices.EXECUTE':
+      execute(reqBody, res);
+      break;
+    default:
+      showError(res, 'Missing intent');
+      break;
+  }
 };
+
+function sync(reqBody, res) {
+  let resBody = {
+    requestId: reqBody.requestId,
+    payload: {
+      devices: [{
+        id: 'lightbulb',
+        type: 'action.devices.types.LIGHT',
+        traits: [
+          'action.devices.traits.OnOff',
+          'action.devices.traits.ColorSpectrum'
+        ],
+        name: {
+          name: 'Lightbulb'
+        },
+        willReportState: true
+      }]
+    }
+  };
+  res.status(200).json(resBody);
+}
+
+function query(reqBody, res) {
+  getDevicesDataFromFirebase(res, devices => {
+    let resBody = {
+      requestId: reqBody.requestId,
+      payload: {
+        devices: {
+          'lightbulb': {
+            on: devices['lightbulb'].on,
+            online: true,
+            color: {
+              spectrumRGB: devices['lightbulb'].spectrumRGB
+            }
+          }
+        }
+      }
+    };
+    res.status(200).json(resBody);
+  });
+}
+
+function execute(reqBody, res) {
+  getDevicesDataFromFirebase(res, devices => {
+    let deviceIdsToUpdate = new Set();
+    let bodyCommands = [];
+
+    reqBody.inputs[0].payload.commands.forEach(command => {
+      command.execution.forEach(execution => {
+        command.devices.forEach(device => {
+          let deviceId = device.id;
+          let success = true;
+
+          switch (execution.command) {
+            case 'action.devices.commands.OnOff':
+              devices[deviceId].on = execution.params.on;
+              break;
+            case 'action.devices.commands.ColorAbsolute':
+              devices[deviceId].spectrumRGB = execution.params.color.spectrumRGB;
+              break;
+            default:
+              success = false;
+              break;
+            }
+
+            if (success) {
+              deviceIdsToUpdate.add(deviceId);
+              bodyCommands.push({ids: [ deviceId ], status: 'SUCCESS'});
+            } else {
+              bodyCommands.push({ids: [ deviceId ], status: 'ERROR'});
+            }
+        });
+      });
+    });
+
+    persistDevicesDataToFirebase(devices, deviceIdsToUpdate);
+
+    let resBody = {
+      requestId: reqBody.requestId,
+      payload: {
+        commands: bodyCommands
+      }
+    };
+    res.status(200).json(resBody);
+  });
+}
+
+function getDevicesDataFromFirebase(res, action) {
+  var devicesRef = db.collection(`users/${userId}/devices`);
+
+  devicesRef.get()
+    .then(snapshot => {
+      let devices = new Object();
+      snapshot.forEach(doc => {
+        devices[doc.id] = doc.data();
+      });
+      action(devices);
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+      showError(res, 'Error getting devices data');
+    });
+}
+
+function persistDevicesDataToFirebase(data, deviceIdsToUpdate) {
+  deviceIdsToUpdate.forEach(deviceId => {
+    db.doc(`users/${userId}/devices/${deviceId}`).set(data[deviceId]);
+  });
+}
+
+function showError(res, message) {
+  res.status(401).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+  }).json({error: message});
+}
